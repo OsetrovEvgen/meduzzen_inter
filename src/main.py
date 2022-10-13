856018ad171bfb6e3e83f9a35bcbe3ff6963721a
Osetrov E <rabota87890012@gmail.com>
2022-10-13 11:48:34 +0300

    corrected remarks

--- src/main.py
+++ src/main.py
@@ -1,28 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 from redis import Redis
+from .config import settings
 from sqlalchemy import create_engine
+from .connection import create_redis_connection, create_postgres_connection
+
 
 from healthcheck.app import router
-from config import settings
 
-
-async def create_redis_connection():
-    if Redis(host=settings.REDIS_HOST,
-        port=settings.REDIS_PORT,
-        password=settings.REDIS_PASSWORD).ping():
-        print("Redis connection accomplished")
-
-async def create_postgres_connection():
-    create_engine("postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}".format(
-            settings.POSTGRES_USER,
-            settings.POSTGRES_PASSWORD,
-            settings.POSTGRES_HOST,
-            settings.POSTGRES_PORT,
-            settings.POSTGRES_DB
-        )
-    )
-    print("PostgreSQL connection accomplished")
 
 def create_application() -> FastAPI:
     application = FastAPI()
