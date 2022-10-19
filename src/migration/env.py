f4b020c2bfd119eab0db4f8db638e50632cf529f
Osetrov E <rabota87890012@gmail.com>
2022-10-19 12:24:28 +0300

    test

--- src/migration/env.py
+++ src/migration/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
+from meduzzen_inter.src.db import base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,7 +17,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from ..db import base
+
 target_metadata = base.metadata
 # target_metadata = None
 
